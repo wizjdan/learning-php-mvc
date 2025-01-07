ea954820162c72d64e0494331e55ace99b60a70c
wizjdan <pangestunugraha1@gmail.com>
2025-01-07 15:21:43 +0700

    Menambahkan file htacess dan memperbaiki routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,7 +3,22 @@
 class App {
 
     public function __construct(){
-        echo "OK!";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL(){
+        //Jika ada URL yang dikirimkan
+        if (isset($_GET["url"])){
+            //rtrim untuk membersihkan tanda / di akhir
+            $url = rtrim($_GET["url"], "/");
+            //Membersihkan url dari karakter yang aneh
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            //Pecah url berdasarkan tanda /
+            $url = explode("/", $url);
+            return $url;
+        }
+
     }
 
 }
