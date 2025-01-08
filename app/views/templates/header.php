19e77e75e62f730ac346dd3065e0039c56146f7b
wizjdan <pangestunugraha1@gmail.com>
2025-01-08 11:18:01 +0700

    Menambahkan Navbar

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,6 +4,22 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $data["halaman"]; ?></title>
-    <link href="http://localhost/phpmvc/public/css/bootstrap.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="<?= BASEURL; ?>/css/bootstrap.css" rel="stylesheet" crossorigin="anonymous">
   </head>
   <body>
+    <!-- Navbar Start -->
+     <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Lerning PHP MVC</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">About</a>
+            </div>
+            </div>
+        </div>
+    </nav>
+     <!-- Navbar End -->
