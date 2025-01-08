22d5d915bf703bcb3574353630cb837fa4154672
wizjdan <pangestunugraha1@gmail.com>
2025-01-08 11:01:43 +0700

    Menambahakan file css dan js dari Bootstrap

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,8 +1,9 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $data["halaman"]; ?></title>
-</head>
-<body>
+    <link href="http://localhost/phpmvc/public/css/bootstrap.css" rel="stylesheet" crossorigin="anonymous">
+  </head>
+  <body>
