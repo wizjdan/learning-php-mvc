01afe11d74518681b57ae71df16b8ca1ae03857c
wizjdan <pangestunugraha1@gmail.com>
2025-01-09 16:06:15 +0700

    Menambahkan fitur Flash Message

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,7 +1,15 @@
 <div class="container">
     <div class="container mt-4">
+
+    <div class="row">
+      <div class="col-lg-6">
+        <?= Flasher::flash() ?>
+
+      </div>
+    </div>
+
         <div class="row">
-            <div class="col-6">
+            <div class="col-lg-6">
                 <!-- Button trigger modal -->
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
                 Tambah Data Mahasiswa
